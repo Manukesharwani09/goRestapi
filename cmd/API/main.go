452158923dfb9254013dba51ab7b03f0408041c0
Manukesharwani09 <mkesharwani125@gmail.com>
2026-02-14 15:11:02 +0530

    graceful shutdown added

--- cmd/API/main.go
+++ cmd/API/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Manukesharwani09/goRestapi/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	//losd config
+
+	cfg := config.MustLoad()
+	// database steup
+	//ruouter
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	//server setuo
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+	slog.Info("server started", slog.String("addr", cfg.HTTPServer.Addr))
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("server failed to start", err)
+		}
+
+	}()
+	<-done
+
+	slog.Info("shutting serevr")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("server shutdown error", err.Error())
+	}
+	slog.Info("server gracefully stopped")
 }
